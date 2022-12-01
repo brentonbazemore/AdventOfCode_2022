874ef99eb09effd542959ed19ff322c0323ecbf1
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-01 00:04:09 -0500

    Finish d01 - pt1

--- d01/pt1/dist/index.js
+++ d01/pt1/dist/index.js
@@ -26,5 +26,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
-const data = rawData.split('\n');
+const data = rawData.split('\n\n');
+console.log(data);
+const summed = data.map(elf => {
+    const calories = elf.split('\n');
+    let sum = 0;
+    for (let i = 0; i < calories.length; i++) {
+        sum += +calories[i];
+    }
+    return sum;
+});
+console.log(Math.max(...summed));
 //# sourceMappingURL=index.js.map
