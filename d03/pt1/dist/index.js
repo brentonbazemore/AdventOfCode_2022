04464ffc72e2f99ca222999ea4d88bffa210060e
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-03 00:09:45 -0500

    Finish d03 - pt1

--- d03/pt1/dist/index.js
+++ d03/pt1/dist/index.js
@@ -24,7 +24,24 @@ var __importStar = (this && this.__importStar) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
+const _ = __importStar(require("lodash"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data = rawData.split('\n');
+const letters = {};
+for (let i = 0; i < 26; i++) {
+    letters[String.fromCharCode(97 + i)] = i + 1;
+}
+for (let i = 0; i < 26; i++) {
+    letters[String.fromCharCode(65 + i)] = i + 1 + 26;
+}
+let total = 0;
+data.forEach((ruckSack) => {
+    const half = Math.ceil(ruckSack.length / 2);
+    const left = ruckSack.slice(0, half).split('');
+    const right = ruckSack.slice(half, ruckSack.length).split('');
+    const dupe = _.intersection(left, right)[0];
+    total += letters[dupe];
+});
+console.log(total);
 //# sourceMappingURL=index.js.map
