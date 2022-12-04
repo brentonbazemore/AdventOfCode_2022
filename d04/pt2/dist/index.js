cb5a963db85a5d99c24d369e363e044c3a02265c
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-04 00:16:53 -0500

    Finish d04 - pt2

--- d04/pt2/dist/index.js
+++ d04/pt2/dist/index.js
@@ -31,12 +31,7 @@ const out = data.filter((pair) => {
     const [left, right] = pair.split(',');
     const [l1, l2] = left.split('-').map(n => +n);
     const [r1, r2] = right.split('-').map(n => +n);
-    if (l1 <= r1 && l2 >= r2) {
-        return true;
-    }
-    if (r1 <= l1 && r2 >= l2) {
-        return true;
-    }
+    return l1 <= r2 && r1 <= l2;
 });
 console.log(out.length);
 //# sourceMappingURL=index.js.map
