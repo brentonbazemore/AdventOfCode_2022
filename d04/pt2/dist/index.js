f06dadfd4ac73301aabc96023aadee3bcf211a4b
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-04 00:05:23 -0500

    Init d04 - pt2

---
+++ d04/pt2/dist/index.js
@@ -0,0 +1,42 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+const inputFile = process.argv[2];
+const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
+const data = rawData.split('\n');
+const out = data.filter((pair) => {
+    const [left, right] = pair.split(',');
+    const [l1, l2] = left.split('-').map(n => +n);
+    const [r1, r2] = right.split('-').map(n => +n);
+    if (l1 <= r1 && l2 >= r2) {
+        return true;
+    }
+    if (r1 <= l1 && r2 >= l2) {
+        return true;
+    }
+});
+console.log(out.length);
+//# sourceMappingURL=index.js.map
