e643c36eb60c8d1ceff54f48ba9b924676dc17c3
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-04 00:05:22 -0500

    Finish d04 - pt1

--- d04/pt1/dist/index.js
+++ d04/pt1/dist/index.js
@@ -27,4 +27,16 @@ const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data = rawData.split('\n');
+const out = data.filter((pair) => {
+    const [left, right] = pair.split(',');
+    const [l1, l2] = left.split('-').map(n => +n);
+    const [r1, r2] = right.split('-').map(n => +n);
+    if (l1 <= r1 && l2 >= r2) {
+        return true;
+    }
+    if (r1 <= l1 && r2 >= l2) {
+        return true;
+    }
+});
+console.log(out.length);
 //# sourceMappingURL=index.js.map
--- d04/pt1/index.ts
+++ d04/pt1/index.ts
@@ -3,3 +3,19 @@ import * as fs from 'fs';
 const inputFile = process.argv[2];
 const rawData: string = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data: string[] = rawData.split('\n');
+
+const out = data.filter((pair) => {
+  const [left, right] = pair.split(',');
+  const [l1, l2] = left.split('-').map(n => +n);
+  const [r1, r2] = right.split('-').map(n => +n);
+
+  if (l1 <= r1 && l2 >= r2) {
+    return true;
+  }
+
+  if (r1 <= l1 && r2 >= l2) {
+    return true;
+  }
+});
+
+console.log(out.length);
