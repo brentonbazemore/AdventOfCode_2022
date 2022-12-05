adf751dcdd5e54222f2239c63e55b0a0ed583a45
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-05 00:19:17 -0500

    Finish d05 - pt1

--- d05/pt1/dist/index.js
+++ d05/pt1/dist/index.js
@@ -26,5 +26,32 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
-const data = rawData.split('\n');
+const [initial, rawInstructions] = rawData.split('\n\n').map(r => r.split('\n'));
+const stacks = {};
+for (let i = 0; i < initial.length - 1; i++) {
+    const row = initial[i];
+    let stackId = 0;
+    for (let j = 1; j < row.length; j += 4) {
+        stackId++;
+        const crate = row[j];
+        if (!stacks[stackId]) {
+            stacks[stackId] = [];
+        }
+        if (crate === ' ') {
+            continue;
+        }
+        stacks[stackId].push(crate);
+    }
+}
+Object.values(stacks).map(s => s.reverse());
+const instructions = rawInstructions.map(inst => {
+    const [, amount, , from, , to] = inst.split(' ').map(Number);
+    return { amount, from, to };
+});
+instructions.forEach(inst => {
+    for (let i = 0; i < inst.amount; i++) {
+        stacks[inst.to].push(stacks[inst.from].pop());
+    }
+});
+console.log(Object.values(stacks).map(s => s[s.length - 1]).join(''));
 //# sourceMappingURL=index.js.map
--- d05/pt1/index.ts
+++ d05/pt1/index.ts
@@ -2,4 +2,36 @@ import * as fs from 'fs';
 
 const inputFile = process.argv[2];
 const rawData: string = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
-const data: string[] = rawData.split('\n');
+const [initial, rawInstructions]: string[][] = rawData.split('\n\n').map(r => r.split('\n'));
+
+const stacks: {[id: number]: string[]} = {};
+for (let i = 0; i < initial.length - 1; i++) {
+  const row = initial[i];
+  let stackId = 0;
+  for (let j = 1; j < row.length; j += 4) {
+    stackId++;
+    const crate = row[j];
+    if (!stacks[stackId]) {
+      stacks[stackId] = [];
+    }
+    if (crate === ' ') {
+      continue;
+    }
+    stacks[stackId].push(crate);
+  }
+}
+
+Object.values(stacks).map(s => s.reverse());
+
+const instructions = rawInstructions.map(inst => {
+  const [, amount, , from, ,to] = inst.split(' ').map(Number);
+  return { amount, from, to }
+});
+
+instructions.forEach(inst => {
+  for (let i = 0; i < inst.amount; i++) {
+    stacks[inst.to].push(stacks[inst.from].pop()!);
+  }
+});
+
+console.log(Object.values(stacks).map(s => s[s.length - 1]).join(''));
