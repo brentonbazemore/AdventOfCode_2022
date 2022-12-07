d1aeee30dcd30a38ae8de10543fbd7de3d3c0d2d
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-07 00:47:17 -0500

    Finish d07 - pt1

--- d07/pt1/dist/index.js
+++ d07/pt1/dist/index.js
@@ -27,4 +27,57 @@ const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data = rawData.split('\n');
+const files = {
+    children: {
+        '/': {
+            children: {},
+            size: 0,
+        }
+    },
+    size: 0,
+};
+let pwd = [];
+data.forEach((cmd) => {
+    console.log(cmd);
+    if (cmd.slice(0, 4) === '$ cd') {
+        const dest = cmd.split('$ cd ')[1];
+        if (dest === '..') {
+            pwd.pop();
+        }
+        else if (dest === '/') {
+            pwd = ['/'];
+        }
+        else {
+            pwd.push(dest);
+        }
+    }
+    else if (cmd.slice(0, 4) === '$ ls') {
+        // handle ls - do nothing?
+    }
+    else if (cmd.slice(0, 3) === 'dir') {
+        const newDir = cmd.split('dir ')[1];
+        let directory = files;
+        pwd.forEach((level) => {
+            directory = directory.children[level];
+        });
+        directory.children[newDir] = { children: {}, size: 0 };
+    }
+    else {
+        const size = +cmd.split(' ')[0];
+        let directory = files;
+        pwd.forEach((level) => {
+            directory = directory.children[level];
+            directory.size += size;
+        });
+    }
+});
+const matches = [...JSON.stringify(files).matchAll(/"size":[0-9]+}/g)];
+let total = 0;
+matches.forEach(m => {
+    const size = +m[0].split(':')[1].split('}')[0];
+    if (size <= 100000) {
+        total += size;
+    }
+});
+console.log(total);
 //# sourceMappingURL=index.js.map
