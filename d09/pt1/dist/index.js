10ff47ba681de8c92ead2bbf65b70826b9f4b2c9
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-09 00:32:13 -0500

    Finish d09 - pt1

--- d09/pt1/dist/index.js
+++ d09/pt1/dist/index.js
@@ -27,4 +27,62 @@ const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data = rawData.split('\n');
+let head = { x: 0, y: 0 };
+let tail = { x: 0, y: 0 };
+const checkAdjacency = (a, b) => {
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            if (a.x + i === b.x && a.y + j == b.y) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+const coords2key = (coords) => {
+    return `${coords.x}_${coords.y}`;
+};
+const visited = {};
+const moveTail = () => {
+    if (!checkAdjacency(head, tail)) {
+        if (head.x !== tail.x) {
+            tail.x += Math.sign(head.x - tail.x);
+        }
+        if (head.y !== tail.y) {
+            tail.y += Math.sign(head.y - tail.y);
+        }
+    }
+};
+const move = {
+    U: () => {
+        head.y++;
+        moveTail();
+    },
+    D: () => {
+        head.y--;
+        moveTail();
+    },
+    R: () => {
+        head.x++;
+        moveTail();
+    },
+    L: () => {
+        head.x--;
+        moveTail();
+    },
+};
+data.forEach((rawMove) => {
+    const [d, a] = rawMove.split(' ');
+    const direction = d;
+    const amount = +a;
+    console.log({ direction, amount });
+    for (let i = 0; i < amount; i++) {
+        move[direction]();
+        console.log('movin', { head, tail });
+        visited[coords2key(tail)] = true;
+    }
+});
+console.log(Object.values(visited).length);
+console.log('final', { head, tail });
+// console.log(checkAdjacency(head, { x: 0, y: 0 }));
 //# sourceMappingURL=index.js.map
