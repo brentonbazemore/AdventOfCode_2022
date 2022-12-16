4b453b29388b5ccf931cebd22d6628b64f5eccee
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-15 23:56:29 -0500

    Init d16 - pt1

---
+++ d16/pt1/index.ts
@@ -0,0 +1,5 @@
+import * as fs from 'fs';
+
+const inputFile = process.argv[2];
+const rawData: string = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
+const data: string[] = rawData.split('\n');
