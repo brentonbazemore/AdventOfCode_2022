4108321c8c411d7757386b8bce1b8e2332d2bea0
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-16 03:56:25 -0500

    Finish d16 - pt1

--- d16/pt1/dist/index.js
+++ d16/pt1/dist/index.js
@@ -27,4 +27,103 @@ const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data = rawData.split('\n');
+const valves = {};
+data.forEach(rawValve => {
+    const [, name, , , rawRate, , , , , ...children] = rawValve.replaceAll(',', '').split(' ');
+    const rate = +rawRate.split('=')[1].split(';')[0];
+    valves[name] = { name, rate, children };
+});
+// premap each of the working valves to each other so that I can cut out the middle steps
+const getNextValve = (valveId) => {
+    const next = valves[valveId].children;
+    return next;
+};
+const findShortestDistance = (start, end) => {
+    const queue = [{ position: start, distance: 0 }];
+    const seen = new Set();
+    while (queue.length > 0) {
+        const { position, distance } = queue.shift();
+        if (position === end) {
+            return distance;
+        }
+        for (const nextPosition of getNextValve(position)) {
+            if (seen.has(nextPosition)) {
+                continue;
+            }
+            seen.add(nextPosition);
+            queue.push({ position: nextPosition, distance: distance + 1 });
+        }
+    }
+    return -1;
+};
+const distances = {};
+const valveArray = Object.values(valves);
+for (let i = 0; i < valveArray.length; i++) {
+    const valveA = valveArray[i];
+    for (let j = 0; j < valveArray.length; j++) {
+        if (i === j) {
+            continue;
+        }
+        const valveB = valveArray[j];
+        if (valveA.rate === 0 || valveB.rate === 0) {
+            if (valveA.name === 'AA') {
+                if (valveB.rate === 0) {
+                    continue;
+                }
+            }
+            else {
+                continue;
+            }
+        }
+        if (!distances[valveA.name]) {
+            distances[valveA.name] = {};
+        }
+        distances[valveA.name][valveB.name] = findShortestDistance(valveA.name, valveB.name) + 1; // +1 is activation cost
+    }
+}
+console.log(JSON.stringify(distances, null, 2));
+const makeDecision = (valve, decisions, timeLeft) => {
+    // console.log({valve, decisions })
+    // if (timeLeft >= 25) {
+    //   console.log(decisions);
+    // }
+    // console.log(decisions);
+    if (timeLeft <= 0) {
+        return 0;
+    }
+    let totalRate = 0;
+    // const lastDecision = decisions.slice(-2);
+    // const lastLast = decisions.slice(-5, -3);
+    // const [a, b] = lastDecision.split('->')!;
+    // if (a === b) {
+    totalRate += (valve.rate * timeLeft);
+    // }
+    // if (valve.rate > 0) {
+    //   const nextDecision = valve.name;
+    //   if (!decisions.includes(nextDecision)) {
+    //     // possibleDecisions.push({ valve, nextDecision, timeConsumed: 1 });
+    //     return totalRate + makeDecision(valve, decisions + ',' + nextDecision, timeLeft - 1);
+    //   }
+    // }
+    const possibleDecisions = [];
+    // console.log('valve.name', valve.name);
+    const childrenIds = Object.keys(distances[valve.name]);
+    for (let i = 0; i < childrenIds.length; i++) {
+        const childId = childrenIds[i];
+        const nextDecision = childId;
+        if (decisions.includes(nextDecision)) {
+            continue;
+        }
+        if (timeLeft - distances[valve.name][childId] < 0) {
+            continue;
+        }
+        possibleDecisions.push({ valve: valves[childId], nextDecision, timeConsumed: distances[valve.name][childId] });
+    }
+    if (possibleDecisions.length > 0) {
+        totalRate += Math.max(...possibleDecisions.map(dec => makeDecision(dec.valve, decisions + ',' + dec.nextDecision, timeLeft - dec.timeConsumed)));
+    }
+    // known[decisions] = totalRate;
+    return totalRate;
+};
+console.log(makeDecision(valves['AA'], 'AA', 30));
 //# sourceMappingURL=index.js.map
--- d16/pt1/index.ts
+++ d16/pt1/index.ts
@@ -3,3 +3,109 @@ import * as fs from 'fs';
 const inputFile = process.argv[2];
 const rawData: string = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data: string[] = rawData.split('\n');
+
+type Valve = { name: string, rate: number, children: string[] };
+const valves: {[name: string]: Valve } = {};
+data.forEach(rawValve => {
+  const [,name,,,rawRate,,,,,...children] = rawValve.replaceAll(',', '').split(' ');
+  const rate = +rawRate.split('=')[1].split(';')[0];
+  valves[name] =  { name, rate, children };
+});
+
+const known: {[key: string]: number} = {};
+const makeDecision = (valve: Valve, decisions: string, timeLeft: number): number => {
+  // console.log({valve, decisions })
+  if (timeLeft >= 25) {
+    console.log(decisions);
+  }
+  if (timeLeft <= 0) {
+    return 0;
+  }
+
+  // if (known[decisions] != undefined) {
+  //   return known[decisions];
+  // }
+
+  let totalRate = 0;
+  const lastDecision = decisions.slice(-6);
+  
+  const [a, b] = lastDecision.split('->')!;
+  if (a === b) {
+    totalRate += (valve.rate * timeLeft);
+  }
+
+  const possibleDecisions = [];
+  if (valve.rate > 0) {
+    const nextDecision = `${valve.name}->${valve.name}`;
+    if (!decisions.includes(nextDecision)) {
+      possibleDecisions.push({ valve, nextDecision });
+    }
+  }
+
+  for (let i = 0; i < valve.children.length; i++) {
+    const child = valve.children[i];
+    const nextDecision = `${valve.name}->${child}`;
+    if (decisions.includes(nextDecision)) {
+      continue;
+    }
+
+    if (a === child && b === valve.name) {
+      // Don't go back unless you've turned on a valve
+      continue;
+    }
+
+    possibleDecisions.push({ valve: valves[child], nextDecision });
+  }
+
+  if (possibleDecisions.length > 0) {
+    totalRate += Math.max(...possibleDecisions.map(dec => makeDecision(dec.valve, decisions + ',' + dec.nextDecision, timeLeft - 1)));
+  }
+
+  // known[decisions] = totalRate;
+
+  return totalRate;
+}
+
+console.log(makeDecision(valves['AA'], 'AA->AA', 30) + 2);
+
+
+// const getNextDecision = (valveId: string) => {
+//   const next = valves[valveId].children.map(child => `${valveId}->${child}`);
+//   next.push(`${valveId}->${valveId}`);
+//   return next;
+// }
+
+// let maxPressureRelease = 0;
+// const bfs = () => {
+//   const queue = [{ decision: 'AA->AA', totalPressure: 0, timeLeft: 30 }];
+//   const seen = new Set<string>();
+
+//   while (queue.length > 0) {
+//     const { decision, totalPressure, timeLeft } = queue.shift()!;
+//     maxPressureRelease = Math.max(maxPressureRelease, totalPressure);
+//     if (timeLeft <= 0) {
+//       console.log('yo');
+//       continue;
+//     }
+
+//     const currentValve = decision.split('->')[1];
+//     for (const nextDecision of getNextDecision(currentValve)) {
+//       if (seen.has(`${nextDecision}@${timeLeft}`)) {
+//         continue; 
+//       }
+//       seen.add(`${nextDecision}@${timeLeft}`);
+
+//       const [valveA, valveB] = nextDecision.split('->');
+//       if (valveA === valveB) {
+//         console.log({valveA, valveB})
+//         queue.push({ decision: nextDecision, totalPressure: totalPressure + (valves[valveA].rate * timeLeft), timeLeft: timeLeft - 1 }); 
+//       }
+//       queue.push({ decision: nextDecision, totalPressure, timeLeft: timeLeft - 1 });
+//     }
+//   }
+
+//   return -1; // 9. If you didn't find the answer, return something like -1/null/undefined.
+// }
+
+// console.log(bfs());
+// console.log(maxPressureRelease);
