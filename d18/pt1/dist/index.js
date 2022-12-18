9aee96945a2e4e988a28196eb6251e3edfa6883f
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-18 00:30:40 -0500

    Finish d18 - pt1

--- d18/pt1/dist/index.js
+++ d18/pt1/dist/index.js
@@ -27,4 +27,47 @@ const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data = rawData.split('\n');
+// build out the object, then find the surface area visible from each of the six sides
+// on second thought, maybe just keep track of exposed surface until while adding
+var Axes;
+(function (Axes) {
+    Axes[Axes["UP"] = 0] = "UP";
+    Axes[Axes["DOWN"] = 1] = "DOWN";
+    Axes[Axes["LEFT"] = 2] = "LEFT";
+    Axes[Axes["RIGHT"] = 3] = "RIGHT";
+    Axes[Axes["FRONT"] = 4] = "FRONT";
+    Axes[Axes["BACK"] = 5] = "BACK";
+})(Axes || (Axes = {}));
+const lava = {};
+data.forEach(coord => {
+    const [x, y, z] = coord.split(',').map(Number);
+    let sides = 6;
+    if (lava[`${x + 1},${y},${z}`] != undefined) {
+        lava[`${x + 1},${y},${z}`]--;
+        sides--;
+    }
+    if (lava[`${x - 1},${y},${z}`] != undefined) {
+        lava[`${x - 1},${y},${z}`]--;
+        sides--;
+    }
+    if (lava[`${x},${y + 1},${z}`] != undefined) {
+        lava[`${x},${y + 1},${z}`]--;
+        sides--;
+    }
+    if (lava[`${x},${y - 1},${z}`] != undefined) {
+        lava[`${x},${y - 1},${z}`]--;
+        sides--;
+    }
+    if (lava[`${x},${y},${z + 1}`] != undefined) {
+        lava[`${x},${y},${z + 1}`]--;
+        sides--;
+    }
+    if (lava[`${x},${y},${z - 1}`] != undefined) {
+        lava[`${x},${y},${z - 1}`]--;
+        sides--;
+    }
+    lava[coord] = sides;
+});
+const surfaceArea = Object.values(lava).reduce((prev, curr) => prev + curr, 0);
+console.log(surfaceArea);
 //# sourceMappingURL=index.js.map
