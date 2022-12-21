b2ec16d3bfb62f8ac404b0810ace095da0187aaf
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-20 23:19:31 -0500

    Finish  - pt1

--- d20/pt1/dist/index.js
+++ d20/pt1/dist/index.js
@@ -26,5 +26,101 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
-const data = rawData.split('\n');
+const data = rawData.split('\n').map(Number);
+const order = rawData.split('\n').map(Number);
+const list = {};
+let first;
+let prev;
+let zero;
+for (let i = 0; i < data.length; i++) {
+    const num = data[i];
+    const id = `${i}_${num}`;
+    if (i === 0) {
+        list[id] = {
+            id,
+            prev: null,
+            value: num,
+            next: null,
+        };
+        first = list[id];
+    }
+    else {
+        list[id] = {
+            id,
+            prev: prev.id,
+            value: num,
+            next: null,
+        };
+        prev.next = id;
+    }
+    if (num === 0) {
+        zero = list[id];
+    }
+    prev = list[id];
+    if (i === data.length - 1) {
+        list[id].next = first.id;
+        first.prev = id;
+    }
+}
+const getArray = (start) => {
+    let next = start;
+    const values = [];
+    for (let i = 0; i < Object.keys(list).length; i++) {
+        values.push(list[next].value);
+        next = list[next].next;
+    }
+    return values;
+};
+const print = () => {
+    console.log(JSON.stringify(getArray(first.id)));
+};
+const removeNode = (key) => {
+    const currNext = list[key].next;
+    const currPrev = list[key].prev;
+    list[currPrev].next = currNext;
+    list[currNext].prev = currPrev;
+};
+for (let i = 0; i < order.length; i++) {
+    const shift = order[i];
+    const key = `${i}_${shift}`;
+    if (shift % order.length === 0) {
+        continue;
+    }
+    removeNode(key);
+    let target;
+    if (shift > 0) {
+        target = key;
+        for (let j = 0; j < shift; j++) {
+            target = list[target].next;
+        }
+    }
+    else if (shift < 0) {
+        target = list[key].prev;
+        for (let j = 0; j < Math.abs(shift); j++) {
+            target = list[target].prev;
+        }
+    }
+    else {
+        // do not move 0; shouldn't even be possible to reach here
+    }
+    if (target == null) {
+        throw new Error("Bad state");
+    }
+    const next = list[target].next;
+    list[target].next = key;
+    list[next].prev = key;
+    list[key].next = next;
+    list[key].prev = target;
+    // console.log({key, target});
+    // print();
+    // console.log('\n')
+    // break;
+}
+const ar = getArray(zero.id);
+// console.log(JSON.stringify(ar));
+const one = ar[1000 % ar.length];
+const two = ar[2000 % ar.length];
+const three = ar[3000 % ar.length];
+console.log({ one, two, three });
+console.log(one + two + three);
 //# sourceMappingURL=index.js.map
