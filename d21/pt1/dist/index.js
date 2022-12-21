24d8c52d89403f83b727dd8787577001e7ed82e4
Brenton Bazemore <brenton.bazemore@gmail.com>
2022-12-21 00:21:51 -0500

    Finish d21 - pt1

--- d21/pt1/dist/index.js
+++ d21/pt1/dist/index.js
@@ -27,4 +27,44 @@ const fs = __importStar(require("fs"));
 const inputFile = process.argv[2];
 const rawData = fs.readFileSync(inputFile || 'inputTest.txt', 'utf8');
 const data = rawData.split('\n');
+const operations = {
+    '*': (a, b) => a * b,
+    '+': (a, b) => a + b,
+    '-': (a, b) => a - b,
+    '/': (a, b) => a / b,
+};
+const monkeys = {};
+data.forEach(rawMonkey => {
+    const [id, rawOperation] = rawMonkey.split(': ');
+    const opTokens = rawOperation.split(' ');
+    if (opTokens.length === 1) {
+        monkeys[id] = {
+            type: 'yeller',
+            value: +opTokens[0],
+        };
+    }
+    else {
+        monkeys[id] = {
+            type: 'doer',
+            value: null,
+            operation: operations[opTokens[1]],
+            dep1: opTokens[0],
+            dep2: opTokens[2],
+        };
+    }
+});
+const solve = (id) => {
+    const monkey = monkeys[id];
+    if (monkey.type === 'yeller') {
+        return monkey.value;
+    }
+    if (monkey.value != null) {
+        return monkey.value;
+    }
+    const dep1 = solve(monkey.dep1);
+    const dep2 = solve(monkey.dep2);
+    monkey.value = monkey.operation(dep1, dep2);
+    return monkey.value;
+};
+console.log(solve('root'));
 //# sourceMappingURL=index.js.map
